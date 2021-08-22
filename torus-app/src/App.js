b860c87ecc3c8b72cfaaa1b9dc813a4b32bd30f4
nondominant <peter_hooper@hotmail.co.nz>
2021-08-23 03:54:09 +1200

    Revert "revert needed"
    This reverts commit 599416606063a3b0eff9f18c3cfc00df3a88f5e8.

--- torus-app/src/App.js
+++ torus-app/src/App.js
@@ -48,6 +48,9 @@ function App() {
 		Test
             </button>
 		{test && <div><strong>Ethereum network Address</strong>: {ethAddress}</div>}
+		{test &&<div><strong>solana cluster connection</strong>: {connection} </div>}
+		{test &&<div><strong>solana Wallet info</strong>: {solanaWallet}</div>}
+		{test &&<div><strong>solana Account info</strong>: {solanaAccount}</div>}
 	   </div>
         ) : (
           <>
